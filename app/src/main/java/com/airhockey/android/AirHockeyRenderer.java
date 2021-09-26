a355fadacfa86a9546872d85ed37d514992c7ffd
digiwizkid <subhadiplayek@gmail.com>
2021-09-26 16:05:03 +0530

    Using Orthogonal Projection to fix aspect ratio problem

--- app/src/main/java/com/airhockey/android/AirHockeyRenderer.java
+++ app/src/main/java/com/airhockey/android/AirHockeyRenderer.java
@@ -17,9 +17,7 @@ import javax.microedition.khronos.opengles.GL10;
 import static android.opengl.GLES20.GL_COLOR_BUFFER_BIT;
 import static android.opengl.GLES20.GL_FLOAT;
 import static android.opengl.GLES20.GL_LINES;
-import static android.opengl.GLES20.GL_LINE_LOOP;
 import static android.opengl.GLES20.GL_POINTS;
-import static android.opengl.GLES20.GL_TRIANGLES;
 import static android.opengl.GLES20.GL_TRIANGLE_FAN;
 import static android.opengl.GLES20.glClear;
 import static android.opengl.GLES20.glClearColor;
@@ -27,10 +25,11 @@ import static android.opengl.GLES20.glDrawArrays;
 import static android.opengl.GLES20.glEnableVertexAttribArray;
 import static android.opengl.GLES20.glGetAttribLocation;
 import static android.opengl.GLES20.glGetUniformLocation;
-import static android.opengl.GLES20.glUniform4f;
+import static android.opengl.GLES20.glUniformMatrix4fv;
 import static android.opengl.GLES20.glUseProgram;
 import static android.opengl.GLES20.glVertexAttribPointer;
 import static android.opengl.GLES20.glViewport;
+import static android.opengl.Matrix.orthoM;
 
 public class AirHockeyRenderer implements GLSurfaceView.Renderer {
 
@@ -44,8 +43,12 @@ public class AirHockeyRenderer implements GLSurfaceView.Renderer {
 
     private static final String A_COLOR = "a_Color";
     private static final int COLOR_COMPONENT_COUNT = 3;
-    private static final int STRIDE = (POSITION_COMPONENT_COUNT +COLOR_COMPONENT_COUNT) * BYTES_PER_FLOAT;
+    private static final int STRIDE = (POSITION_COMPONENT_COUNT + COLOR_COMPONENT_COUNT) * BYTES_PER_FLOAT;
     private int aColorLocation;
+
+    private static final String U_MATRIX = "u_Matrix";
+    private final float[] projectionMatrix = new float[16];
+    private int uMatrixLocation;
 
     private int program;
 
@@ -53,23 +56,23 @@ public class AirHockeyRenderer implements GLSurfaceView.Renderer {
         this.context = context;
 
         float[] tableVerticesWithTriangles = {
-            // Order of co-ordinates: X, Y, R, G, B
+                // Order of co-ordinates: X, Y, R, G, B
 
-            // Triangle fan
-                0f,   0f,   1f,   1f,   1f,
-            -0.5f, -0.5f, 0.7f, 0.7f, 0.7f,
-             0.5f, -0.5f, 0.7f, 0.7f, 0.7f,
-             0.5f,  0.5f, 0.7f, 0.7f, 0.7f,
-            -0.5f, 0.5f, 0.7f, 0.7f, 0.7f,
-            -0.5f, -0.5f, 0.7f, 0.7f, 0.7f,
+                // Triangle fan
+                0f, 0f, 1f, 1f, 1f,
+                -0.5f, -0.8f, 0.7f, 0.7f, 0.7f,
+                0.5f, -0.8f, 0.7f, 0.7f, 0.7f,
+                0.5f, 0.8f, 0.7f, 0.7f, 0.7f,
+                -0.5f, 0.8f, 0.7f, 0.7f, 0.7f,
+                -0.5f, -0.8f, 0.7f, 0.7f, 0.7f,
 
-            // Line 1
-            -0.5f, 0f, 1f, 0f, 0f,
-             0.5f, 0f, 1f, 0f, 0f,
+                // Line 1
+                -0.5f, 0f, 1f, 0f, 0f,
+                0.5f, 0f, 1f, 0f, 0f,
 
-            // Mallets
-            0f, -0.25f, 0f, 0f, 1f,
-            0f,  0.25f, 1f, 0f, 0f
+                // Mallets
+                0f, -0.25f, 0f, 0f, 1f,
+                0f, 0.25f, 1f, 0f, 0f
         };
 
         vertexData = ByteBuffer
@@ -100,6 +103,7 @@ public class AirHockeyRenderer implements GLSurfaceView.Renderer {
 
         aPositionLocation = glGetAttribLocation(program, A_POSITION);
         aColorLocation = glGetAttribLocation(program, A_COLOR);
+        uMatrixLocation = glGetUniformLocation(program, U_MATRIX);
 
         vertexData.position(0);
         glVertexAttribPointer(aPositionLocation, POSITION_COMPONENT_COUNT, GL_FLOAT, false, STRIDE, vertexData);
@@ -114,20 +118,33 @@ public class AirHockeyRenderer implements GLSurfaceView.Renderer {
     @Override
     public void onSurfaceChanged(GL10 gl10, int width, int height) {
         glViewport(0, 0, width, height);
+
+        final float aspectRatio = width > height ?
+                (float) width / (float) height : (float) height / (float) width;
+
+        if (width > height) {
+            // Landscape
+            orthoM(projectionMatrix, 0, -aspectRatio, aspectRatio, -1f, 1f, -1f, 1f);
+        } else {
+            // Portrait or square
+            orthoM(projectionMatrix, 0, -1f, 1f, -aspectRatio, aspectRatio, -1f, 1f);
+        }
     }
 
     @Override
     public void onDrawFrame(GL10 gl10) {
         glClear(GL_COLOR_BUFFER_BIT);
 
+        glUniformMatrix4fv(uMatrixLocation, 1, false, projectionMatrix, 0);
+
         // Drawing table
         glDrawArrays(GL_TRIANGLE_FAN, 0, 6);
 
         // Drawing line
-        glDrawArrays(GL_LINES, 6,2);
+        glDrawArrays(GL_LINES, 6, 2);
 
         // Drawing the first mallet
-        glDrawArrays(GL_POINTS, 8,1);
+        glDrawArrays(GL_POINTS, 8, 1);
 
         // Drawing the second mallet
         glDrawArrays(GL_POINTS, 9,1);
